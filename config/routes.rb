34be4e18cbbe1facc234dc792275895ef16b3de4
OsamaShehata <engosama46@yahoo.com>
2023-01-21 14:26:54 +0100

    tasks completed, just need to be styled better :D

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,25 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # list all tasks
+  get '/tasks', to: 'tasks#index'
+
+  # new task
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+
+  # show one task
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  # create a new task
+  post '/tasks', to: 'tasks#create'
+
+  # view a task to edit
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+
+  # update a task
+  patch '/tasks/:id', to: 'tasks#update'
+
+  # delete a task
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
